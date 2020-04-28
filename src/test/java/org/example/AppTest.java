d14977d8eec9af72165f703fd865373cbe0b3d40
= <ashillpathak@gmail.com>
2020-04-28 15:31:29 +0100

    added before and after methods

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,10 +2,25 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class AppTest 
 {
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
 
 }
