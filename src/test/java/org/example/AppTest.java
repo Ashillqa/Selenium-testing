6ce0aff423efe05615e2fa9e9bc23340dd49c6c6
= <ashillpathak@gmail.com>
2020-04-29 13:33:12 +0100

    draw a house

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import static java.lang.Thread.*;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.sql.SQLOutput;
 
@@ -27,17 +28,26 @@ public class AppTest
     public void seleniumExampleTest() throws InterruptedException{
         driver.manage().window().maximize();
         sleep(2000);
-        driver.get("http://www.google.co.uk");
+        driver.get("https://www.youidraw.com/apps/painter/");
         sleep(1000);
-        WebElement googleSearchBar = driver.findElement(By.name("q"));
-        googleSearchBar.sendKeys("funny dog pics");
-        sleep(1000);
-        googleSearchBar.submit();
-        WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
-        linkToPictures.click();
-        WebElement imageHighlight = driver.findElement(By.className("NZmxZe"));
-        assertTrue(imageHighlight.isDisplayed());
-        sleep(1000);
+
+        Actions action = new Actions(driver);
+        action.moveByOffset(600,250).clickAndHold().moveByOffset(125,50)
+                .moveByOffset(0,187).moveByOffset(-250,0).moveByOffset(0,-187)
+                .moveByOffset(125,-50).release().moveByOffset(-125,50).clickAndHold().moveByOffset(250,0).release()
+                .moveByOffset(-225,50).clickAndHold().moveByOffset(75,0).moveByOffset(0,50).
+                moveByOffset(-75,0).moveByOffset(0,-50).release().moveByOffset(125,0).
+                clickAndHold().moveByOffset(75,0).moveByOffset(0,50).moveByOffset(-75,0).
+                moveByOffset(0,-50).release().moveByOffset(0,137).clickAndHold().moveByOffset(0,-50).
+                moveByOffset(-50,0).moveByOffset(0,50).release().perform();
+
+
+
+
+        sleep(3000);
+
+
+
     }
 
     @After
